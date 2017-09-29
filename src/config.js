de749f2cf4ac720bc534e0962ef8be783679c2ad
Antonio V <tokageroh@gmail.com>
2017-09-29 15:13:54 +0900

    Refactor main code.

--- src/config.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// default conf
-module.exports = {
-  host: 'localhost:9200'
-};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 'use strict';
 const elasticsearch = require('elasticsearch');
-const defaultConfig = require('./config.js');
 
 exports.bootstrap = (index, type, options) => {
   return new Loader(index, type, options);
@@ -11,37 +10,26 @@ class Loader {
    * Create instance
    * Possible config can be checked here:
    * https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/configuration.html
-   *
    * @param {string} index - index by default used in the operations
    * @param {string} type - type by default used in the operations
    * @param {Object} [config] - config object
-   *
    */
   constructor (index, type, config) {
-    // Create an ES instance
-    const options = Object.assign({}, defaultConfig, config);
-    this.client = new elasticsearch.Client(options);
+    this.client = new elasticsearch.Client(config);
     this.index = index;
     this.type = type;
   }
 
   /**
-   * Add/index documents
-   *
+   * Index documents
    * @param {Object[]} data - must be in the format specified in the bulk API
    * @param {Object} [options]
-   * @param {boolean} [options.incremental] - by default assign a random _id
+   * @param {boolean} [options.incremental] - assign an incremental id instead of a random one (Default: false)
+   * @param {boolean} [options.noRefresh] - avoid refreshing the index on each iteration
    * @param {Function} [callback] - only in case callback style is used
-   *
    * @return {Promise}
-   *
    */
   load (data, options, callback) {
-    if (typeof options === 'function') {
-      callback = data;
-      options = undefined;
-    }
-
     const bulkData = [];
     if (options && options.incremental) {
       let count = 1;
@@ -62,58 +50,70 @@ class Loader {
       });
     }
 
-    return this.bulk(bulkData, callback);
+    return this.bulk(bulkData, options, callback);
   }
 
   /**
    * Delete all the documents
-   *
+   * @param {Object} [options]
+   * @param {boolean} [options.noRefresh] - avoid refreshing the index on each iteration
    * @param {Function} [callback] - only in case callback style is used
-   *
    * @return {Promise}
-   *
    */
-  clear (callback) {
+  clear (options, callback) {
     const client = this.client;
-    const loopAndDeleteDocs = response => {
-      // delete all docs obtained in the last scroll search
-      const promises = response.hits.hits.map((hit) => {
-        return client.delete({
-          index: this.index,
-          type: this.type,
-          id: hit._id,
-          refresh: true
+    const index = this.index;
+    const type = this.type;
+
+    /**
+     * Loop through all the docs and delete them using the scroll API
+     * @return {Promise}
+     */
+    const deleteByScroll = () => {
+      return client.search({
+        scroll: '30s',
+        index: index,
+        type: type,
+        sort: '_doc:asc',
+        fields: []
+      }).then(function loopAndDeleteDocs(response) {
+        const promises = response.hits.hits.map((hit) => {
+          return client.delete({
+            index: index,
+            type: type,
+            id: hit._id,
+            refresh: !(options && options.noRefresh)
+          });
         });
-      });
-      return Promise.all(promises)
-      // get next batch once the docs have been deleted
-      .then(() => {
-        return client.scroll({
-          scrollId: response._scroll_id,
-          scroll: '30s'
+        return Promise.all(promises).then(() => {
+          return client.scroll({
+            scrollId: response._scroll_id,
+            scroll: '30s'
+          });
+        }).then(response => {
+          // call this function recursively until all docs have been deleted
+          if (response.hits.hits.length > 0) {
+            return loopAndDeleteDocs(response);
+          }
+          if (callback) {
+            callback();
+          }
         });
-      })
-      // call this function recursively until all docs have been deleted
-      .then(response => {
-        if (response.hits.hits.length > 0) {
-          return loopAndDeleteDocs(response);
-        }
-        if (callback) {
-          callback();
-        }
       });
     };
 
-    // initial search request, specifying the scroll parameter
-    return client.search({
-      scroll: '30s',
-      index: this.index,
-      type: this.type,
-      fields: []
-    })
-    // loop through all the docs
-    .then(loopAndDeleteDocs)
-    .catch(err => {
+    return client.info().then(info => {
+      const version = info.version.number;
+      if (version.slice(0,1) === '2') {
+        return deleteByScroll();
+      } else {
+        return client.deleteByQuery({
+          index: index,
+          type: type,
+          refresh: !(options && options.noRefresh)
+        });
+      }
+    }).catch(err => {
       if (callback) {
         return callback(err);
       }
@@ -123,55 +123,40 @@ class Loader {
 
   /**
    * Delete all documents and add new ones
-   *
    * @param {Object[]} data - must be in the format specified in the bulk API
    * @param {Object} [options]
-   * @param {boolean} [options.incremental] - by default assign a random _id
+   * @param {boolean} [options.incremental] - assign an incremental id instead of a random one
+   * @param {boolean} [options.noRefresh] - avoid refreshing the index on each iteration
    * @param {Function} [callback] - only in case callback style is used
-   *
    * @return {Promise}
-   *
    */
   clearAndLoad (data, options, callback) {
-    if (typeof options === 'function') {
-      callback = data;
-      options = undefined;
-    }
-
-    return this.clear()
-    .then(() => this.load(data, options, callback));
+    return this.clear().then(() =>
+      this.load(data, options, callback)
+    );
   }
 
   /**
-   * Delete index and re-create it setting optional mappings
-   *
-   * @param {Object} [data] - must be in the format specified
+   * Perform operations using bulk API
+   * More info: https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-bulk.html
+   * @param {Object[]} data - must be in the format specified in the bulk API
+   * @param {Object} [options]
+   * @param {boolean} [options.noRefresh] - avoid refreshing the index on each iteration
    * @param {Function} [callback] - only in case callback style is used
-   *
    * @return {Promise}
-   *
    */
-  recreateIndex (data, callback) {
-    if (typeof data === 'function') {
-      callback = data;
-      data = undefined;
-    }
-    return this.client.indices.delete({
+  bulk (data, options, callback) {
+    return this.client.bulk({
       index: this.index,
-      ignore: [404]
-    })
-    .then(() => {
-      return this.client.indices.create({
-        index: this.index,
-        body: data
-      });
-    })
-    .then(() => {
+      type: this.type,
+      body: data,
+      refresh: !(options && options.noRefresh)
+    }).then(result => {
       if (callback && typeof callback === 'function') {
-        callback();
+        return callback(null, result);
       }
-    })
-    .catch(err => {
+      return result;
+    }).catch(err => {
       if (callback && typeof callback === 'function') {
         return callback(err);
       }
@@ -180,29 +165,31 @@ class Loader {
   }
 
   /**
-   * Perform operations using bulk API
-   * More info: https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-bulk.html
-   *
-   * @param {Object[]} data - must be in the format specified in the bulk API
+   * Create index with optional settings
+   * @param {Object} [data] - optional settings, must be in the format specified
+   * @param {Object} [options]
+   * @param {boolean} [options.force] - force creation, delete index if exist
    * @param {Function} [callback] - only in case callback style is used
-   *
    * @return {Promise}
-   *
    */
-  bulk (data, callback) {
-    return this.client.bulk({
-      index: this.index,
-      type: this.type,
-      body: data,
-      refresh: true
-    })
-    .then(result => {
+  createIndex (data, options, callback) {
+    let p = Promise.resolve();
+    if (options && options.force) {
+      p = this.client.indices.delete({
+        index: this.index,
+        ignore: [404]
+      });
+    }
+    return p.then(() => {
+      return this.client.indices.create({
+        index: this.index,
+        body: data
+      });
+    }).then(() => {
       if (callback && typeof callback === 'function') {
-        return callback(null, result);
+        callback();
       }
-      return result;
-    })
-    .catch(err => {
+    }).catch(err => {
       if (callback && typeof callback === 'function') {
         return callback(err);
       }
@@ -212,20 +199,16 @@ class Loader {
 
   /**
    * Add mapping to specified type
-   *
    * @param {Object} data - mapping data
    * @param {Function} [callback] - only in case callback style is used
-   *
    * @return {Promise}
-   *
    */
   addMapping (data, callback) {
     return this.client.indices.putMapping({
       index: this.index,
       type: this.type,
       body: data
-    })
-    .catch(err => {
+    }).catch(err => {
       if (callback && typeof callback === 'function') {
         return callback(err);
       }
